c48afa8e44406b14054e0e154f852a51a80b5aa2
Milton Sanchez <snchzmltn@hotmail.com>
2021-08-01 18:33:39 -0500

    adds Header component introduce material UI

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { AppBar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import { AccountCircle } from '@material-ui/icons';
+
+function Header() {
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+
+	const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<AppBar position="fixed" color="primary">
+			<Toolbar>
+				<IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenu}>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h6" >
+					Menu
+          			</Typography>
+				<Menu
+					id="menu-appbar"
+					anchorEl={anchorEl}
+					anchorOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					keepMounted
+					transformOrigin={{
+						vertical: 'top',
+						horizontal: 'right',
+					}}
+					open={open}
+					onClose={handleClose}
+				>
+					<MenuItem onClick={handleClose}>My Profile</MenuItem>
+					<MenuItem onClick={handleClose}>Contact Me</MenuItem>
+				</Menu>
+			</Toolbar>
+		</AppBar>
+	);
+}
+
+export default Header;
