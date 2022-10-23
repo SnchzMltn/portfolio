fd909ad16eb975266113ef013087f169c5e60a3d
Milton Sanchez <snchzmltn@hotmail.com>
2022-10-23 02:59:05 -0500

    abstracted cardcontent component (actually a preserved word) and made the app reactive with a json feed in a static file
    two new static files added with sensitive info

--- src/components/AboutMeContent/AboutMeContent.tsx
+++ src/components/AboutMeContent/AboutMeContent.tsx
@@ -1,8 +1,14 @@
-import { Accordion, AccordionDetails, AccordionSummary, Typography, Card, CardContent, CardActionArea, Box } from '@material-ui/core';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, Card, CardActionArea, Box } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import React, { useState } from 'react';
 import './AboutMeContent.css';
+import { XP } from '../../static/xp.js';
+import { PROFESSIONAL_PROFILE } from '../../static/constants';
+import CardContent from '../CardContent/CardContent';
 
 function AboutMeContent() {
+	const [toggle, setToggle] = useState(false)
+	const [displayDetails, setDisplayDetails] = useState([{}]);
 
 	return (
 		<div className="AboutMeContent">
@@ -17,32 +23,29 @@ function AboutMeContent() {
 					</Typography>
 				</AccordionSummary>
 				<AccordionDetails>
-					<Box sx={{maxWidth: '50%'}}>
-						<Card variant="outlined" >
-						<CardActionArea>
-							<CardContent>
-								<Typography variant="h2" component="div" className="AccorAccordionDetailsTypographyTitle">
-									Technical Support Engineer - Twilio, Inc
-								</Typography>
-								<Typography>
-									Several tasks performed, mainly focused on providing support in Levels 1, 2 and occasionally 3, within a 6-member team in charge of technically supporting the account security products developed and maintained in Colombia.
-									End-user support is provided, aswell as developer and personalized (custom) support for implementations of the Authy API in order to provide MFA solutions for applications already on production stage. Several software solutions are used on a daily basis including:
-									<ul>
-										<li>Kibana</li>
-										<li>DataDog</li>
-										<li>PagerDuty</li>
-										<li>Slack</li>
-										<li>Postman</li>
-										<li>Ngrok</li>
-										<li>Development tools</li>
-									</ul>
-									<h3>
-										April 23th 2018 - 01 October
-									</h3>
-								</Typography>
-							</CardContent>
-						</CardActionArea>
-					</Card>
+					<Box sx={{ maxWidth: '90%' }}>
+						{XP.map(object => {
+							return (<Card variant="outlined">
+								<CardActionArea key={object.index} onClick={() => {
+									setToggle(!toggle)
+									setDisplayDetails(prevState => {
+										let newState: {}[]
+										if (prevState[object.index]) {
+											delete prevState[object.index]
+											newState = { ...prevState, [object.index]: !prevState[object.index] }
+										} else {
+											newState = { ...prevState, [object.index]: toggle }
+										}
+										return { ...newState }
+									})
+								}}>
+									<Typography variant="h5" component="div" className="AccorAccordionDetailsTypographyTitle">
+										{object.role} - {object.company}
+									</Typography>
+									{displayDetails[object.index] ? <CardContent role={object.role} company={object.company} description={object.description} tools={object.tools} extra_tools={object.extra_tools || []} dates={object.dates} /> : null}
+								</CardActionArea>
+							</Card>)
+						})}
 					</Box>
 				</AccordionDetails>
 			</Accordion>
@@ -53,13 +56,13 @@ function AboutMeContent() {
 					id="panel1a-header"
 				>
 					<Typography variant="h2">
-						Proyects
+						Professional profile
 					</Typography>
 				</AccordionSummary>
 				<AccordionDetails>
 					<Typography>
-						Graduated professional from the EAN University with a degree in Systems Engineering. Skills in functional programming, and advanced knowledge in Object Oriented Programming, specifically on design and abstraction. With knowledge of full-stack elements suchas MVVC architecture and database administration. High familiarity with different languages such as Java, C#, Python PL-SQL, PHP and also with different development environments (IDEs) such as Eclipse, Visual Studio 2017, .NET, PLSQL Developer and Yii. Experience in developing applications for Android and familiarity with data structures, algorithms and version management tools such as GIT and TFS. Excellent interpersonal skills and problem-solving oriented as well as a history with unique and original solutions throughout creativity. Consolidating more than three years of experience working in the complete software lifecycle including planning, consultancy, customer requirements definition, functional design, software development, testing, quality assurance (QA), implementation and maintenance.
-          				</Typography>
+						{PROFESSIONAL_PROFILE}
+					</Typography>
 				</AccordionDetails>
 			</Accordion>
 		</div>
---
+++ src/components/CardContent/CardContent.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { CardContent as CardContentC, Typography } from "@material-ui/core";
+
+function CardContent(object: { role: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; company: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; description: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; tools: (boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | String[] | null | undefined)[]; extra_tools: any[]; dates: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) {
+	return (
+		<CardContentC>
+			
+			<Typography>
+				{object.description}
+
+				<ul>
+					<h3>Main tools used:</h3>
+					{object.tools.map((tool: boolean | Array<String> | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) => {
+						return (
+							<li>{tool}</li>
+						)
+					})}
+				</ul>
+
+				{object.extra_tools?.map((extra_tool: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined) => {
+					return (
+						<div>
+							<ul>
+								<li>{extra_tool}</li>
+							</ul>
+						</div>
+					)
+				})}
+
+				<h3>{object.dates}</h3>
+			</Typography>
+		</CardContentC>
+	)
+}
+
+export default CardContent;
