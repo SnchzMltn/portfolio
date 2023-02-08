6dcc850eb27cd8d5a2f2ab995f0b0c2a80e9ded7
Milton Sanchez <snchzmltn@hotmail.com>
2023-02-08 04:12:47 -0500

    create constants for theme colors and actually used those with Material UI ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,38 @@ import Header from './components/header/Header';
 import AboutMeContent from './components/AboutMeContent/AboutMeContent';
 import ContactFormContent from './components/ContactFormContent/ContactFormContent';
 import './App.css';
+import { COLORS } from './static/constants';
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
 
 function App() {
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: COLORS.main,
+      },
+      secondary: {
+        main: COLORS.secondary,
+      }
+    },
+  });
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<AboutMeContent />} />
-          <Route path='/contact/*' element={<ContactFormContent />} />
-        </Routes>
-        <Header />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<AboutMeContent />} />
+            <Route path='/contact/*' element={<ContactFormContent />} />
+          </Routes>
+          <Header />
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
   );
 }
--- src/static/constants.js
+++ src/static/constants.js
@@ -1,3 +1,8 @@
 const PROFESSIONAL_PROFILE = "Graduated professional from the EAN University with a degree in Systems Engineering. Skills in functional programming, and advanced knowledge in Object Oriented Programming, specifically on design and abstraction. With knowledge of full-stack elements suchas MVVC architecture and database administration. High familiarity with different languages such as Java, C#, Python PL-SQL, PHP and also with different development environments (IDEs) such as Eclipse, Visual Studio 2017, .NET, PLSQL Developer and Yii. Experience in developing applications for Android and familiarity with data structures, algorithms and version management tools such as GIT and TFS. Excellent interpersonal skills and problem-solving oriented as well as a history with unique and original solutions throughout creativity. Consolidating more than three years of experience working in the complete software lifecycle including planning, consultancy, customer requirements definition, functional design, software development, testing, quality assurance (QA), implementation and maintenance."
 
-export { PROFESSIONAL_PROFILE }
+const COLORS = {
+	main: '#6096B4',
+	secondary: '#93BFCF',
+}
+
+export { PROFESSIONAL_PROFILE, COLORS }
