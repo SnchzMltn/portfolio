9dc60cd25e088e780893ded009a523923e0f0e72
Milton Sanchez <snchzmltn@hotmail.com>
2023-08-20 19:32:29 -0500

    update styles add icon for skills

--- src/pages/AboutMeContent/AboutMeContent.tsx
+++ src/pages/AboutMeContent/AboutMeContent.tsx
@@ -4,6 +4,16 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './AboutMeContent.css';
 
 import prof_img from '../../static/img_prof_2048.jpeg';
+
+import javaLogo from '../../static/icons/java.svg';
+import pythonLogo from '../../static/icons/python.svg';
+import sqlLogo from '../../static/icons/sql.svg';
+import mongoLogo from '../../static/icons/mongodb.svg';
+import reactLogo from '../../static/icons/react.svg';
+import html5Logo from '../../static/icons/html5.svg';
+import awsLogo from '../../static/icons/aws.svg';
+import googleLogo from '../../static/icons/google.svg';
+
 import { PROFESSIONAL_PROFILE } from '../../static/constants';
 
 import Timeline from '../../components/Timeline/Timeline';
@@ -12,17 +22,18 @@ type Skill = {
 	name: string,
 	rate: number,
 	progress: number,
+	logo: any,
 }
 
 const skillsArray: Skill[] = [
-	{name: "Java (JDK 8+)", rate: 5, progress: 100},
-	{name: "Python", rate: 4, progress: 80},
-	{name: "Relational Database (SQL)", rate: 5, progress: 100}, 
-	{name: "Non-relational Database (NoSQL)", rate: 4, progress: 80}, 
-	{name: "React", rate: 3.5, progress: 75}, 
-	{name: "HTML5/CSS3", rate: 5, progress: 100},
-	{name: "Amazon Web Services", rate: 4, progress: 80},
-	{name: "Google Cloud Platform", rate: 3.5, progress: 75},
+	{name: "Java (JDK 8+)", rate: 5, progress: 100, logo: javaLogo},
+	{name: "Python", rate: 4, progress: 80, logo: pythonLogo},
+	{name: "Relational Database (SQL)", rate: 5, progress: 100, logo: sqlLogo}, 
+	{name: "Non-relational Database (NoSQL)", rate: 4, progress: 80, logo: mongoLogo}, 
+	{name: "React", rate: 3.5, progress: 75, logo: reactLogo}, 
+	{name: "HTML5/CSS3", rate: 5, progress: 100, logo: html5Logo},
+	{name: "Amazon Web Services", rate: 4, progress: 80, logo: awsLogo},
+	{name: "Google Cloud Platform", rate: 3.5, progress: 75, logo: googleLogo},
 ]
 
 function AboutMeContent() {
@@ -64,14 +75,22 @@ function AboutMeContent() {
 								}}  spacing={2}>
 									<>
 										<ListItem>
-											<Typography variant='body2' noWrap >
-												{skill.name}
-											</Typography>
-											<Box width='50%' marginLeft='auto' marginRight='5%'>
-												<LinearProgress sx={{
-													marginTop: 'auto',
-													marginBottom: 'auto',
-												}} variant='determinate' value={skill.progress} />
+											<Box width='100%'>
+												<Stack justifyContent='left' direction='row' spacing={2}>
+													<Box width='10%'>
+														<img alt={skill.name} width='24px' height='24px' src={skill.logo} style={{
+															margin: '5px',
+														}} />
+													</Box>
+													<Box width='40%' display='flex' alignItems='center'>
+														<Typography  variant='body2' noWrap >
+															{skill.name}
+														</Typography>
+													</Box>
+													<Box paddingTop='1.5%' alignItems='right' width='50%'>
+														<LinearProgress variant='determinate' value={skill.progress} />
+													</Box>
+												</Stack>
 											</Box>
 										</ListItem>
 									</>
