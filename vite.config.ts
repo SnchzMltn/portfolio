768f84a7fc4025c6f68e3494983fe7ddac1c0d20
Milton Sanchez <snchzmltn@hotmail.com>
2026-07-12 00:04:05 -0500

    include vue sitemap plugin no deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     allowedHosts: ['miltonsanchez.com', '.miltonsanchez.com', 'localhost'],
   },
-  plugins: [react()],
+  plugins: [
+    react(), Sitemap({ 
+      hostname: 'https://miltonsanchez.com',
+      // basePath: '/',
+      dynamicRoutes: ['/about', '/contact'],
+    }),
+  ],
   build: {
     cssMinify: true,
     minify: 'esbuild',
@@ -14,6 +21,6 @@ export default defineConfig({
       input: {
         main: './index.html',
       }
-    }
+    },
   }
 })
