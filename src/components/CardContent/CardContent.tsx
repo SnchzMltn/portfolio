ad469992cf572dcde774a651c7227e6998db383d
Milton Sanchez <snchzmltn@hotmail.com>
2026-07-12 20:29:27 -0500

    formatting

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -1,37 +1,37 @@
 import { Typography, CardContent as CardContentC } from "@mui/material";
-// import React from "react";
-// import { CardContent as CardContentC, Typography } from "@material-ui/core";
 
-function CardContent(object: { description: string; tools: string[]; extra_tools?: string[]; dates: string; }) {
-	return (
-		<CardContentC>
-			
-			<Typography>
-				{object.description}
+function CardContent(object: {
+  description: string;
+  tools: string[];
+  extra_tools?: string[];
+  dates: string;
+}) {
+  return (
+    <CardContentC>
+      <Typography>
+        {object.description}
 
-				<ul>
-					<h3>Main tools used:</h3>
-					{object.tools.map(tool => {
-						return (
-							<li key={tool}>{tool}</li>
-						)
-					})}
-				</ul>
+        <ul>
+          <h3>Main tools used:</h3>
+          {object.tools.map((tool) => {
+            return <li key={tool}>{tool}</li>;
+          })}
+        </ul>
 
-				{object.extra_tools?.map(extraTool => {
-					return (
-						<div>
-							<ul>
-								<li key={extraTool}>{extraTool}</li>
-							</ul>
-						</div>
-					)
-				})}
+        {object.extra_tools?.map((extraTool) => {
+          return (
+            <div>
+              <ul>
+                <li key={extraTool}>{extraTool}</li>
+              </ul>
+            </div>
+          );
+        })}
 
-				<h3>{object.dates}</h3>
-			</Typography>
-		</CardContentC>
-	)
+        <h3>{object.dates}</h3>
+      </Typography>
+    </CardContentC>
+  );
 }
 
 export default CardContent;
